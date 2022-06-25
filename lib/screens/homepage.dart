55d8d37808f1dad1c86b5369a21b9741eace6d4b
jana <janamurugesan01@gmail.com>
2022-06-25 21:14:26 +0530

    Added size config file and children profile part created in Homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:classchool/utils/constants.dart';
+import 'package:classchool/utils/size_config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -15,6 +16,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -44,12 +46,87 @@ class _HomePageState extends State<HomePage> {
             IconButton(
               onPressed: () {},
               icon: const Icon(
-                Icons.category,
-                // jana you can use this icon da (grid_view_rounded)
+                Icons.grid_view_rounded,
               ),
               iconSize: 30,
             )
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              margin: const EdgeInsets.all(20),
+              height: SizeConfig.screenHeight! * 0.3,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "CHILDREN PROFILES",
+                        style: GoogleFonts.jost(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        childrenProfile(
+                            imageUrl:
+                                'https://images.unsplash.com/photo-1630178836733-3d61d8974258?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+                            name: 'ArunKumar'),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        childrenProfile(
+                            imageUrl:
+                                'https://images.unsplash.com/photo-1630178836733-3d61d8974258?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+                            name: 'ArunKumar'),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        childrenProfile(
+                            imageUrl:
+                                'https://images.unsplash.com/photo-1630178836733-3d61d8974258?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+                            name: 'ArunKumar'),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        childrenProfile(
+                            imageUrl:
+                                'https://images.unsplash.com/photo-1630178836733-3d61d8974258?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+                            name: 'ArunKumar'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              height: SizeConfig.screenHeight! * 0.6,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ]),
         ),
         bottomNavigationBar: Container(
           margin: const EdgeInsets.all(20),
@@ -97,4 +174,38 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+Widget childrenProfile({required String name, required String imageUrl}) {
+  return Container(
+    height: SizeConfig.screenHeight! * 0.22,
+    width: 120,
+    decoration: BoxDecoration(
+      color: Color(0xFFF6F1FF),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ClipOval(
+          child: Image.network(
+            imageUrl,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(name,
+              style: GoogleFonts.jost(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500),
+              maxLines: 2,
+              textAlign: TextAlign.center),
+        ),
+      ],
+    ),
+  );
 }
